93ec0604d2914f5eb28d1da57161e2ebe19ee449
SmallMing675 <181466198+smallming675@users.noreply.github.com>
2025-05-11 16:48:54 +0800

    Fix

--- app.js
+++ app.js
@@ -6,25 +6,9 @@ import { parse_input } from "./public/parsing.js";
 import { run_recipe } from "./public/oceu.js";
 import { generate_table } from "./public/gen_table.js";
 import { parse_duration } from "./public/util.js";
+
 const app = express();
-
-const privateKey = readFileSync("localhost-key.pem", "utf8");
-const certificate = readFileSync("localhost.pem", "utf8");
-
-const passphrase = "bruh";
-const credentials = { key: privateKey, passphrase, cert: certificate };
-
-const httpsServer = createServer(credentials, app);
-
-function ensureSecure(req, res, next) {
-  if (req.secure) {
-    return next();
-  }
-  res.redirect("https://" + req.hostname + req.originalUrl);
-}
-
-app.use(ensureSecure);
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -45,8 +29,4 @@ app.post("/api/gen_table", (req, res) => {
 // Start the server
 app.listen(port, () => {
   console.log(`API running on http://localhost:${port}`);
-});
-
-httpsServer.listen(443, () => {
-  console.log("HTTPS server running on port 443");
 });
